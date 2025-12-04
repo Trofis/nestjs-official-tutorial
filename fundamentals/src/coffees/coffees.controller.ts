753c77859ec0eba0ce24cb81978b056a19516633
trofis <porto.thomas@gmail.com>
2025-12-04 09:04:50 +0100

    feat(fundamentals): add timeout interceptor

--- fundamentals/src/coffees/coffees.controller.ts
+++ fundamentals/src/coffees/coffees.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Inject } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Inject,
+} from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { CreateCoffeeDto } from './dto/create.coffee.dto';
@@ -9,34 +19,42 @@ import { Public } from 'src/common/decorators/public.decorators';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(
+    private readonly coffeesService: CoffeesService,
+    @Inject(REQUEST) private readonly request: Request,
+  ) {
+    console.log('CoffeesController constructor');
+  }
 
-    constructor(private readonly coffeesService: CoffeesService, @Inject(REQUEST) private readonly request: Request) {
-        console.log('CoffeesController constructor')
-    }
+  @Public()
+  @Get()
+  async findAll(
+    @Query() paginationQuery: PaginationQueryDto,
+  ): Promise<Coffee[]> {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    return this.coffeesService.findAll(paginationQuery);
+  }
 
-    @Public()
-    @Get()  
-    async findAll(@Query() paginationQuery: PaginationQueryDto): Promise<Coffee[]> {
-        return this.coffeesService.findAll(paginationQuery)
-    }
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Coffee> {
+    return this.coffeesService.findOne(id);
+  }
 
-    @Get(':id')
-    async findOne(@Param('id') id: string): Promise<Coffee> {
-        return this.coffeesService.findOne(id)
-    }
+  @Post()
+  async create(@Body() createCoffeeDto: CreateCoffeeDto): Promise<Coffee> {
+    return this.coffeesService.create(createCoffeeDto);
+  }
 
-    @Post()
-    async create(@Body() createCoffeeDto: CreateCoffeeDto): Promise<Coffee> {
-        return this.coffeesService.create(createCoffeeDto)
-    }
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateCoffeeDto: UpdateCoffeeDto,
+  ): Promise<Coffee> {
+    return this.coffeesService.update(id, updateCoffeeDto);
+  }
 
-    @Patch(':id')
-    async update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto): Promise<Coffee> {
-        return this.coffeesService.update(id, updateCoffeeDto)
-    }
-
-    @Delete(':id')
-    async remove(@Param('id') id: string): Promise<string> {
-        return this.coffeesService.remove(id)
-    }
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<string> {
+    return this.coffeesService.remove(id);
+  }
 }
